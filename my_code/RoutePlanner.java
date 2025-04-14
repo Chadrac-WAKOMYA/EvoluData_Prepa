252ce68fa7af45413b38289c761266629c2a7aa7
unknown <chadracxx@gmail.com>
2025-04-15 01:42:26 +0200

    Yesy depart et destinqtion

--- my_code/RoutePlanner.java
+++ my_code/RoutePlanner.java
@@ -3,7 +3,21 @@ package my_code;
 public class RoutePlanner {
     public static boolean routeExists(int fromRow, int fromColumn, int toRow, int toColumn,
                                       boolean[][] mapMatrix) {
-        throw new UnsupportedOperationException("Waiting to be implemented.");
+        // throw new UnsupportedOperationException("Waiting to be implemented.");
+        // Tester si le départ et l'arrivé sont dans mapMatrix 
+        if(fromRow < 0 || fromRow >= mapMatrix.length ||
+        fromColumn < 0 || fromColumn >= mapMatrix[0].length ||
+        toRow < 0 || toRow >= mapMatrix.length ||
+        toColumn < 0 || toColumn >= mapMatrix[0].length
+        ){
+            return false;
+        }
+
+        // Tester si le départ et l'arrivé  sont vrais
+        if(!mapMatrix[fromRow][fromColumn] || !mapMatrix[toRow][toColumn]){
+            return false; // Départ ou destination impossible
+        }
+        return  false;
     }
         
     public static void main(String[] args) {
