891ba1f3266a417b7b893f543452ef3f6ad66232
unknown <chadracxx@gmail.com>
2025-04-15 15:51:34 +0200

    ready to continue

--- my_code/RoutePlanner.java
+++ my_code/RoutePlanner.java
@@ -36,11 +36,14 @@ public class RoutePlanner {
         // Déclarations de variables importantes 
         Set <String> routeDejaParcourue = new HashSet<>(); // Variale qui contenir toutes les routes déjà vérifiées
         List <String> nouveauaDepart  = new ArrayList<>(); // Variable qui contenir les nouveaux départs
+        nouveauaDepart. add("mapMatrix[fromRow][fromColumn]");
+
         Queue<Map<String, String>> possiblesRoutesAVerifier = new LinkedList<>(); // Variable possibles routes à vérifier
         Map <String, Map<String, String>> routeAExplorer = new HashMap<>(); // La variable qui va prendre les prochaines routes à explorer
 
         // La boucle de recherche de nouvelles possibilités
         for(String routeChoisie :  nouveauaDepart){
+            if(routeDejaParcourue.contains(routeAExplorer))
             while (!possiblesRoutesAVerifier.isEmpty()) {
             
             }
