e87dc3962c8b313560050e64a5eff991f6f88791
unknown <chadracxx@gmail.com>
2025-04-21 06:49:09 +0200

    Game PlateForme done

---
+++ my_code/GamePlatforme.java
@@ -0,0 +1,17 @@
+package my_code;
+
+public class GamePlatforme {
+    public static double calculateFinalSpeed(double initialSpeed, int[] inclinations) {
+        double finalSpeed = initialSpeed;
+        for(int nbr : inclinations){
+            finalSpeed -= nbr;
+            if(finalSpeed <= 0)
+                return 0;
+        }
+        return finalSpeed;
+    }
+
+    public static void main(String[] args){
+        System.out.println(calculateFinalSpeed(60.0, new int[] { 0, 30, 0, -45, 0 }));
+    }
+}
